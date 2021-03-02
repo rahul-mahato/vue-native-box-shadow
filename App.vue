9a1c36c0892f6849f91e9ab2762af49f7d7b137d
Rahul Mahato <rahulm@geekyants.com>
2021-03-02 19:04:17 +0530

    Feat: Added Shadow to the View boxes

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="container">
+    <view class="shadow-box" :style="styles.shadowBox" />
+  </view>
+</template>
+<script>
+import { StyleSheet } from 'react-native';
+export default {
+  data: function () {
+    return {
+      styles: new StyleSheet.create({
+        shadowBox: {
+          elevation: 60,
+          shadowOffset: { width: 2, height: 2 },
+          shadowColor: '#000000',
+          shadowOpacity: 0.8,
+          height: 500,
+          backgroundColor: '#000',
+          width: 200,
+          shadowRadius: 50,
+        },
+      }),
+    };
+  },
+};
+</script>
+<style>
+.container {
+  flex: 1;
+}
+
+.camera-container {
+  height: 500;
+  width: 500;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
+i
