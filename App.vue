4ce131df94b94e6c93d9d1c8073c010fb391244f
Rahul Mahato <rahulm@geekyants.com>
2021-03-02 20:36:38 +0530

    Chore: Moved Stylesheet Initialisation out of the component.

--- App.vue
+++ App.vue
@@ -3,27 +3,30 @@
     <view class="shadow-box" :style="styles.shadowBox" />
   </view>
 </template>
+
 <script>
 import { StyleSheet } from 'react-native';
+
+const styles = new StyleSheet.create({
+  shadowBox: {
+    elevation: 60,
+    shadowOffset: { width: 2, height: 2 },
+    shadowColor: '#000000',
+    shadowOpacity: 0.8,
+    height: 500,
+    backgroundColor: '#000',
+    width: 200,
+    shadowRadius: 50,
+  },
+});
+
 export default {
-  data: function () {
-    return {
-      styles: new StyleSheet.create({
-        shadowBox: {
-          elevation: 60,
-          shadowOffset: { width: 2, height: 2 },
-          shadowColor: '#000000',
-          shadowOpacity: 0.8,
-          height: 500,
-          backgroundColor: '#000',
-          width: 200,
-          shadowRadius: 50,
-        },
-      }),
-    };
+  data: {
+    styles,
   },
 };
 </script>
+
 <style>
 .container {
   flex: 1;
@@ -37,4 +40,3 @@ export default {
   color: blue;
 }
 </style>
-i
